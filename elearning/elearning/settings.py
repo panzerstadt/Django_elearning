8b729b65898326f3b014256a04d2c73fce4bffcb
Tang Li Qun <tliqun@gmail.com>
2018-02-03 16:04:18 +0900

    lesson19 - procfile (settings for heroku) - runtime.txt (tells heroku which python version to run - whitenoise for serving static files

--- elearning/elearning/settings.py
+++ elearning/elearning/settings.py
@@ -58,6 +58,7 @@ AUTH_USER_MODEL = 'students.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -148,6 +149,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'elearning', 'static'), ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 LOGIN_REDIRECT_URL = '/'
 
