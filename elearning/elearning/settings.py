197117a592fdd8d648b045ffa03eea661ca3e27a
Tang Li Qun <tliqun@gmail.com>
2018-02-03 16:26:29 +0900

    lesson19 - k, runtime.txt: use python-3.6.2  (don't forget to type pythonDASH3.6.2) - remember to update the default runtime after you type heroku create

--- elearning/elearning/settings.py
+++ elearning/elearning/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY',
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
-if DEBUG.lower() in ['off', 'no', 'false', '0', '']:
+if DEBUG in ['Off', 'off', 'No', 'no', 'False', 'false', '0', '']:
     DEBUG = False
 else:
     DEBUG = True
